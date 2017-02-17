52b2ef0bf48f1b2511c1c71f9142031118e1904c
jiangyixin <540872052@qq.com>
2017-02-17 18:09:19 +0800

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <my-header></my-header>
     <router-view></router-view>
+    <footer></footer>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import myHeader from './components/Header'
+  // import Footer from './components/Footer'
+  export default {
+    name: 'app',
+    components: {
+      myHeader
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <a class="index" href="/">Hello World</a>
+    <input class="search" type="search" name="" value="" placeholder="Search...">
+  </div>
+</template>
+<style scoped lang="less" rel="stylesheet/less">
+  @hello: #dd4433;
+  @world: #66bb33;
+  .hello {
+    color: @hello;
+  }
+  .world {
+    color: @world;
+  }
+  .index {
+    display: block;
+    color: #333;
+
+  }
+  .search {
+
+  }
+</style>
+<script>
+
+  export default{
+    data () {
+      return {
+        msg: 'hello vue'
+      }
+    },
+    components: {
+
+    }
+  }
+</script>
