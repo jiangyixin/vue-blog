200c2d84c2d5388e0b42a225f4c1667e1e15a100
jiangyixin <540872052@qq.com>
2017-03-10 17:20:28 +0800

    update eslint and package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,116 @@
 // http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
+  "root": true,
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    }
   },
-  env: {
-    browser: true,
+  "extends": "vue",
+  "plugins": ["flow-vars", "react"],
+  "globals": {
+    "$": true,
+    "define": true,
+    "require": true
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // add your custom rules here
-  'rules': {
-    'import/no-unresolved': 0,
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'no-console': 0,
-    'no-unused-vars': [2, {'args': 'none'}],
-    'semi': [2, 'never'],
-    // 'object-curly-spacing': [2, 'never'],
-    'comma-dangle': [2, 'never'],
-    'no-unused-expressions': [2, {'allowShortCircuit': true, 'allowTernary': true}],
-    'prefer-const': 0,
-    'prefer-template': 0,
-    'prefer-arrow-callback': 0,
-    'max-len': [0, 120],
-    'space-before-function-paren': [1, {'anonymous': 'ignore', 'named': 'always' }],
-    'object-shorthand': [2, 'always', { 'avoidQuotes': true }],
-    'no-param-reassign': [2, { "props": false }],
-    'global-require': 0,
-    'brace-style': [2, 'stroustrup', {'allowSingleLine': true }],
-    'arrow-body-style': [2, 'always'],
-    'func-names': [2, 'never'],
-    'default-case': 0
+  "env": {
+    "browser": true,
+    "node": true,
+    "commonjs": true,
+    "es6": true,
+    "amd": true
+  },
+  "rules": {
+    // 不需要
+    "space-before-function-paren": 0,
+    "eol-last": 0,
+    "no-extra-semi": 0,
+    "semi": 0,
+    "eqeqeq": 0,
+    "one-var": 0,
+    "no-undef": 0,
+    "object-curly-spacing": 0,
+
+    // 警告
+    "no-extra-boolean-cast": 1,
+    "no-extra-parens": 1,
+    "no-empty": 1,
+    "no-use-before-define": [1, "nofunc"],
+    "complexity": [1, 10],
+    "no-unused-vars": 1,
+
+    // vue
+    "flow-vars/define-flow-type": 1,
+    "flow-vars/use-flow-type": 1,
+
+    // react
+    "react/jsx-uses-react": 2,
+    "react/jsx-uses-vars": 2,
+
+    // 错误
+    "comma-dangle": [2, "never"],
+    "no-debugger": 2,
+    "no-constant-condition": 2,
+    "no-dupe-args": 2,
+    "no-dupe-keys": 2,
+    "no-duplicate-case": 2,
+    "no-empty-character-class": 2,
+    "no-invalid-regexp": 2,
+    "no-func-assign": 2,
+    "valid-typeof": 2,
+    "no-unreachable": 2,
+    "no-unexpected-multiline": 2,
+    "no-sparse-arrays": 2,
+    "no-shadow-restricted-names": 2,
+    "no-cond-assign": 2,
+    "no-native-reassign": 2,
+
+    // 代码风格
+    "no-else-return": 1,
+    "no-multi-spaces": 1,
+    "key-spacing": [1, {
+      "beforeColon": false,
+      "afterColon": true
+    }],
+    "block-scoped-var": 2,
+    "consistent-return": 2,
+    "accessor-pairs": 2,
+    "dot-location": [2, "property"],
+    "no-lone-blocks": 2,
+    "no-labels": 2,
+    "no-extend-native": 2,
+    "no-floating-decimal": 2,
+    "no-loop-func": 2,
+    "no-new-func": 2,
+    "no-self-compare": 2,
+    "no-sequences": 2,
+    "no-throw-literal": 2,
+    "no-return-assign": [2, "always"],
+    "no-redeclare": [2, {
+      "builtinGlobals": true
+    }],
+    "no-unused-expressions": [2, {
+      "allowShortCircuit": true,
+      "allowTernary": true
+    }],
+    "no-useless-call": 2,
+    "no-useless-concat": 2,
+    "no-void": 2,
+    "no-with": 2,
+    "space-infix-ops": 2,
+    "valid-jsdoc": [2, {
+      "requireParamDescription": true,
+      "requireReturnDescription": true
+    }],
+    "no-warning-comments": [2, {
+      "terms": ["todo", "fixme", "any other term"],
+      "location": "anywhere"
+    }],
+    "curly": 1,
+
+    // common js
+    "no-duplicate-imports": 1
   }
-}
+};
---
+++ src/assets/js/validate.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Administrator on 2017/3/3.
+ */
+let validate = {
+  notNull: function (value) {
+    return value !== '';
+  },
+  maxLength: function (value, maxLen) {
+    return value.length() < maxLen
+  }
+};
+
+document.addValidate = function(options) {
+ for (let option of options) {
+   if (!validate[option]) {
+     return option;
+   }
+ }
+};
+
+let nameInput = $('#name');
+nameInput.addValidate({
+  notNull: true,
+  maxLength: 30
+});
+
+
+
